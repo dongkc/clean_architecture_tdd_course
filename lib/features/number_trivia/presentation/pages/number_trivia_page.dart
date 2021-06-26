adef92685a92652e304874459661a64f0513693d
dongkechang <dongkechang@nuctech.com>
2021-06-26 13:10:24 +0800

    update

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -9,17 +9,129 @@ import '../../../../injection_container.dart';
 class NumberTriviaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var queryData = MediaQuery.of(context);
+    double devicePixelRatio = queryData.devicePixelRatio;
+    print('pixel ratio: $devicePixelRatio');
+    print('pixel ratio: $queryData');
+    var size = queryData.size;
+    print('pixel width: ${size.width}');
+    print('pixel height: ${size.height}');
     return Scaffold(
       appBar: AppBar(
         title: Text('Number Trivia'),
       ),
-      body: SingleChildScrollView(
-        child: buildBody(context),
-      ),
+      body: buildBody(context),
+      // body: SingleChildScrollView(
+      //   child: buildBody(context),
+      // ),
     );
   }
 
   BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Row(
+        children: [
+          Placeholder(
+            fallbackWidth: 80,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 320,
+            child: Card(
+              child: Column(
+                children: [
+                  Text(
+                    "HW123456788765432",
+                    style: TextStyle(fontSize: 30.0),
+                    textAlign: TextAlign.center,
+                  ),
+                  Table(
+                    columnWidths: {
+                      0: FlexColumnWidth(1),
+                      1: FlexColumnWidth(1),
+                    },
+                    children: [
+                      TableRow(children: [
+                        Text(
+                          "报警等级:",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          "正常",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          "固件版本:",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          "1.0.0",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          "硬件版本:",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          "1.0.0",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          "压力(KN):",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          "120.0",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          "压力AD值:",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          "12222",
+                          style: TextStyle(fontSize: 30.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ])
+                    ],
+                  ),
+                  Placeholder(
+                    color: Colors.red,
+                    fallbackWidth: 200,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Placeholder(
+            color: Colors.green,
+            fallbackWidth: 400,
+          ),
+        ],
+      ),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody2(BuildContext context) {
     return BlocProvider(
       create: (_) => sl<NumberTriviaBloc>(),
       child: Center(
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await di.init();
   runApp(MyApp());
 }
