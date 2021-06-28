49923feaa20ac6676a2523c712fa5431f18c694a
dongkechang <dongkechang@nuctech.com>
2021-06-28 14:42:30 +0800

    update

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,5 +1,6 @@
 import 'package:clean_architecture_tdd_course/features/number_trivia/presentation/bloc/bloc.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/presentation/widgets/IAE203_controls.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -42,115 +43,20 @@ class NumberTriviaPage extends StatelessWidget {
             // child: buildCard1(),
             child: buildCard2(context),
           ),
-          Container(width: 300, child: buildColumn()),
-          Expanded(
-            child: buildContainer(context),
-          ),
+          // Row(
+          //   children: [
+          //     Placeholder(fallbackWidth: 300),
+          //     // Expanded(
+          //       // child: Placeholder(),
+          //     // ),
+          //   ],
+          // ),
+          IAE203Controls(),
+          // Container(width: 300, child: buildColumn()),
+          // Expanded(
+          //   child: buildContainer(context),
+          // ),
         ],
-      ),
-    );
-  }
-
-  Widget buildContainer(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 100,
-            child: Builder(
-              builder: (BuildContext context) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                        child: Container(
-                            margin: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  BlocProvider.of<NumberTriviaBloc>(context)
-                                      .add(IAE203UpdateParamsEvent("test"));
-                                },
-                                child: Text('读取参数')))),
-                    Expanded(
-                        child: Container(
-                            margin: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
-                                onPressed: () {}, child: Text('写入参数')))),
-                  ],
-                );
-              },
-            ),
-          ),
-          Placeholder(
-            color: Colors.blue,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildColumn() {
-    return Card(
-      child: Container(
-        margin: const EdgeInsets.all(4.0),
-        child: Column(
-          children: [
-            Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '生产年月'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '归零值'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '标定值'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '微松动阈值'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '松动阈值'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '过压阈值'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: '唤醒时间'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(labelText: 'IP地址(域名)'),
-                ),
-                TextFormField(
-                  initialValue: '0',
-                  style: TextStyle(fontSize: 25.0),
-                  decoration: InputDecoration(
-                    labelText: '端口号',
-                    // labelStyle: TextStyle(fontSize: 25.0),
-                  ),
-                ),
-              ],
-            )),
-          ],
-        ),
       ),
     );
   }
